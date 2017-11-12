38fb51d3aa584e054a8626565185209248db7a7b
Yohan Gomez <yohan.gz@hotmail.com>
2017-11-12 20:41:33 +0530

    Add example route and ui route

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,7 +1,14 @@
 package controllers
 
 import javax.inject._
+
 import play.api.mvc._
+import play.libs.Json
+
+class AppSummary(val cn: String) {
+  var content: String = cn
+}
+
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -10,14 +17,9 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def appSummary = Action {
+    val jsonNode = Json.toJson(new AppSummary("Java Play Angular Seed"))
+    Ok(jsonNode).as("application/json")
   }
 
 }
