2dcb1d2d75006baf6cac92e1e5910ce58abe873f
Yohan Gomez <yohan.gz@hotmail.com>
2017-11-12 21:43:42 +0530

    Fix controller compile error

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,19 +2,13 @@ package controllers
 
 import javax.inject._
 
+import play.api.libs.json.Json
 import play.api.mvc._
-import play.libs.Json
-
-class AppSummary(val cn: String) {
-  var content: String = cn
-}
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def appSummary = Action {
-    val jsonNode = Json.toJson(new AppSummary("Java Play Angular Seed"))
-    Ok(jsonNode).as("application/json")
+    Ok(Json.obj("content" -> "Java Play Angular Seed"))
   }
-
 }
