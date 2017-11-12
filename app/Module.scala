83837b5b4ef7474798bf09d1d46b98368ef7fd62
Yohan Gomez <yohan.gz@hotmail.com>
2017-11-12 20:33:46 +0530

    Remove example endpoints

--- app/Module.scala
+++ app/Module.scala
@@ -1,7 +1,4 @@
 import com.google.inject.AbstractModule
-import java.time.Clock
-
-import services.{ApplicationTimer, AtomicCounter, Counter}
 
 /**
  * This class is a Guice module that tells Guice how to bind several
@@ -16,13 +13,6 @@ import services.{ApplicationTimer, AtomicCounter, Counter}
 class Module extends AbstractModule {
 
   override def configure() = {
-    // Use the system clock as the default implementation of Clock
-    bind(classOf[Clock]).toInstance(Clock.systemDefaultZone)
-    // Ask Guice to create an instance of ApplicationTimer when the
-    // application starts.
-    bind(classOf[ApplicationTimer]).asEagerSingleton()
-    // Set AtomicCounter as the implementation for Counter.
-    bind(classOf[Counter]).to(classOf[AtomicCounter])
   }
 
 }
