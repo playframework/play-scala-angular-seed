0fbce1df0223fb2bc82d938e66b7724e4a231f9c
Yohan Gomez <yohan.gz@hotmail.com>
2018-01-28 16:01:01 +0530

    Remove index serve from proxy

---
+++ ui/proxy.conf.js
@@ -0,0 +1,14 @@
+const PROXY_CONFIG = {
+  "**": {
+    "target": "http://localhost:9000",
+    "secure": false,
+    "bypass": function (req) {
+      if (req.headers.accept.indexOf("html") !== -1) {
+        console.log("Skipping proxy for browser request.");
+        return "/index.html";
+      }
+    }
+  }
+};
+
+module.exports = PROXY_CONFIG;
